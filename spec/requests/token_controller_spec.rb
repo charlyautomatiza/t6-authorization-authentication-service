0064175c679b616f416917825b11d7f475a18ef7
MauricioNC <mauricionc19@gmail.com>
2024-04-16 15:35:46 -0600

    feat: Updated tests to match with POST request

--- spec/requests/token_controller_spec.rb
+++ spec/requests/token_controller_spec.rb
@@ -3,10 +3,12 @@ require 'json'
 
 RSpec.describe "TokenControllers", type: :request do
   before do
-    @generate_token_endpoint = "http://localhost:3000/api/v1/users/55e70916-8939-4fb3-a3a1-b67e726c26a8/generate_token"
+    @generate_token_endpoint = "http://localhost:3000/api/v1/generate_token"
+    @params = { "id": "55e70916-8939-4fb3-a3a1-b67e726c26a8" }
+    @headers = { "Content-type": "application/json" }
   end
 
-  describe "GET /api/v1/55e70916-8939-4fb3-a3a1-b67e726c26a8/generate_token" do
+  describe "GET /api/v1/generate_token" do
     context "On set_key_and_pass_phrase" do
       it "creates key and pass_phrase" do
         tokenInstance = Api::V1::TokensController.new
@@ -19,7 +21,7 @@ RSpec.describe "TokenControllers", type: :request do
 
     context "On encrypted code fails" do
       it "rsponds with status unprocessable_entity" do
-        get @generate_token_endpoint
+        post @generate_token_endpoint, params: @params.to_json, headers: @haders
 
         expect(response).to have_http_status(:unprocessable_entity)
       end
@@ -37,16 +39,18 @@ RSpec.describe "TokenControllers", type: :request do
 
     context "When user not found" do
       it "responds with unprocessable_entity" do
-        get @generate_token_endpoint
+        post @generate_token_endpoint, params: @params.to_json, headers: @headers
 
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it "responds with unprocessable_entity in the response and error message" do
-        get @generate_token_endpoint
+        post @generate_token_endpoint, params: @params.to_json, headers: @headers
 
-        expect(JSON.parse(response.body)['status']).to eq(422)
-        expect(JSON.parse(response.body)['errors']).to eq("Couldn't find User with 'user_id'=55e70916-8939-4fb3-a3a1-b67e726c26a8")
+        resp_body = HashWithIndifferentAccess.new(JSON.parse(response.body))
+
+        expect(resp_body[:status]).to eq(422)
+        expect(resp_body[:errors]).to eq("Couldn't find User with 'user_id'=55e70916-8939-4fb3-a3a1-b67e726c26a8")
       end
     end
 
@@ -55,10 +59,10 @@ RSpec.describe "TokenControllers", type: :request do
         User.create(user_id: "55e70916-8939-4fb3-a3a1-b67e726c26a8", email: 'mauricionc19@gmail.com', email_confirmed: false, fullname: 'Mauricio Navarro', password: 'Mauricion123', username: 'MauricioNC')
       end
 
-      it "responds with ok and created as status code with success message" do
-        get @generate_token_endpoint
+      it "responds with created as status code with success message" do
+        post @generate_token_endpoint, params: @params.to_json, headers: @headers
 
-        expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(:created)
         expect(JSON.parse(response.body)['status']).to eq(201)
         expect(JSON.parse(response.body)['message']).to eq("Token created successfully assigned to MauricioNC")
       end
