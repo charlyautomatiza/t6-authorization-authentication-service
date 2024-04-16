6eddd687b208d4e7b3de6032fb29a301f1c01594
MauricioNC <mauricionc19@gmail.com>
2024-04-16 12:26:55 -0600

    feat: Added tests to Tokens controller

--- spec/requests/token_controller_spec.rb
+++ spec/requests/token_controller_spec.rb
@@ -7,17 +7,31 @@ RSpec.describe "TokenControllers", type: :request do
   end
 
   describe "GET /api/v1/55e70916-8939-4fb3-a3a1-b67e726c26a8/generate_token" do
-    context "On success response" do
-      before do
-        User.create(user_id: "55e70916-8939-4fb3-a3a1-b67e726c26a8", email: 'mauricionc19@gmail.com', email_confirmed: false, fullname: 'Mauricio Navarro', password: 'Mauricion123', username: 'MauricioNC')
-      end
+    context "On set_key_and_pass_phrase" do
+      it "creates key and pass_phrase" do
+        tokenInstance = Api::V1::TokensController.new
+        tokenInstance.send(:set_key_and_pass_phrase)
 
-      it "responds with ok and success message status code" do
+        expect(tokenInstance.instance_variable_get(:@key).class).to eq(String)
+        expect(tokenInstance.instance_variable_get(:@pass_phrase).class).to eq(String)
+      end
+    end
+
+    context "On encrypted code fails" do
+      it "rsponds with status unprocessable_entity" do
         get @generate_token_endpoint
 
-        expect(response).to have_http_status(:ok)
-        expect(JSON.parse(response.body)['status']).to eq(201)
-        expect(JSON.parse(response.body)['message']).to eq("Token created successfully assigned to MauricioNC")
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "On encrypted code success" do
+      it "creates an encrypted code" do
+        encrypted_code = Api::V1::TokensController.new
+        encrypted_code.send(:set_key_and_pass_phrase)
+        code = encrypted_code.send(:create_encrypted_code)
+
+        expect(code.class).to eq(String)
       end
     end
 
@@ -32,7 +46,21 @@ RSpec.describe "TokenControllers", type: :request do
         get @generate_token_endpoint
 
         expect(JSON.parse(response.body)['status']).to eq(422)
-        expect(JSON.parse(response.body)['errors']).to eq("Couldn't find User with 'user_id'=55e70916-8939-4fb3-a3a1-b67e726c26a9")
+        expect(JSON.parse(response.body)['errors']).to eq("Couldn't find User with 'user_id'=55e70916-8939-4fb3-a3a1-b67e726c26a8")
+      end
+    end
+
+    context "On success response" do
+      before do
+        User.create(user_id: "55e70916-8939-4fb3-a3a1-b67e726c26a8", email: 'mauricionc19@gmail.com', email_confirmed: false, fullname: 'Mauricio Navarro', password: 'Mauricion123', username: 'MauricioNC')
+      end
+
+      it "responds with ok and created as status code with success message" do
+        get @generate_token_endpoint
+
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)['status']).to eq(201)
+        expect(JSON.parse(response.body)['message']).to eq("Token created successfully assigned to MauricioNC")
       end
     end
   end
