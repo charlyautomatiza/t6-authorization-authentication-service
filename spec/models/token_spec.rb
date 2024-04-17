9ae9c64c2cbdb4b5a981885019ba4b42f91ba39a
MauricioNC <mauricionc19@gmail.com>
2024-04-16 21:12:27 -0600

    feat: Added model tests

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
+require 'json'
 
 RSpec.describe Token, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @url = "http://localhost:3000/api/v1/token"
+    @headers = { "Content-type": "application/json" }
+  end
+
+  describe "POST /api/v1/token" do
+    context "When user not found" do
+      before do
+        @body = { "id": "1" }
+      end
+      it "responds with :unprocessable_entity (422) status code" do
+        post @url, params: @body.to_json, header: @headers
+
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "When user exists" do
+      before do
+        @user = User.create(username: "user_test", fullname: "User Test", email: "test@gmail.com", password: "test1234")
+        @body = { "id": @user.id }
+      end
+      it "responds with :created (200) status code" do
+        post @url, params: @body.to_json, header: @headers
+
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
 end
