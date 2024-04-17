ada712b700a7300be96b6045ba148da3607803cb
MauricioNC <mauricionc19@gmail.com>
2024-04-17 13:25:37 -0600

    feat: Create authentication token completed

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -18,6 +18,10 @@ class Api::V1::TokensController < ApplicationController
     render json: { status: 201, message: "Token created successfully assigned to #{@user.username}", token: token }, status: :created
   end
 
+  def create_token
+    generate_authentication_token if params[:token_type] == 'authentication'
+  end
+
   private
 
   def set_user
@@ -30,8 +34,8 @@ class Api::V1::TokensController < ApplicationController
   end
 
   def set_key_and_pass_phrase
-    @key = "apiAuthentication"
-    @pass_phrase = "SecretPhraseToAuthenticateAPIcalls"
+    @key = "keyForTokenAuthorizationAndAuthentication"
+    @pass_phrase = "SecretPhraseToAuthenticateAndAuthorizeAPIcalls"
   end
 
   def create_encrypted_code
@@ -44,5 +48,28 @@ class Api::V1::TokensController < ApplicationController
         e.message
       ]
     }, status: :unprocessable_entity
+  end
+
+  def generate_authentication_token
+    remove_current_token unless @user.token.nil?
+
+    @user.build_token(token_type: params[:token_type])
+
+    if @user.save
+      render json: {
+        status: 201,
+        message: 'Token created successfully',
+        token: @user.token.token
+      }, status: :created
+    end
+  rescue ActiveRecord::RecordNotSaved => e
+    render json: {
+      status: 422,
+      error: e.message
+    }, status: :unprocessable_entity
+  end
+
+  def remove_current_token
+    @user.token.delete
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/generate_token', to: 'tokens#generate_token'
+      post '/tokens/:token_type', to: 'tokens#create_token'
     end
   end
 end
