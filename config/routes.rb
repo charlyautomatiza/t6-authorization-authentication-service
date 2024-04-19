f591ef8a55826102c7fe51caa0461225b5106d31
MauricioNC <mauricionc19@gmail.com>
2024-04-18 18:12:21 -0600

    feat: Refactoring the auth service

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
-      post '/tokens/:token_type', to: 'tokens#create_token'
+      post '/tokens/authentication', to: 'tokens#create_authentication_token'
+      post '/tokens/authorization', to: 'tokens#create_authorization_token'
     end
   end
 end
