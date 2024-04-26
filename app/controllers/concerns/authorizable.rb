a79ab8864cab2f3dc6ac74031d3e840c5764ae4f
MauricioNC <mauricionc19@gmail.com>
2024-04-26 12:53:18 -0600

    feat: Changed JWT token to random string token

--- app/controllers/concerns/authorizable.rb
+++ app/controllers/concerns/authorizable.rb
@@ -2,28 +2,21 @@ module Authorizable
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_key_and_pass_phrase, only: %i[ create_authorization_token authorize ]
-    before_action :create_encrypted_code, only: %i[ create_authorization_token authorize ]
-    before_action :set_options, only: %i[ create_authorization_token authorize ]
-    before_action :set_jwt_body, only: %i[ create_authorization_token ]
+    before_action :set_key_and_pass_phrase, only: %i[ create_authorization_token ]
+    before_action :generate_token, only: %i[ create_authorization_token ]
   end
 
   def authorize
-    token = request.headers['Authorization']
-    user = decode(token, @options)
+    auht_token = request.headers['Authorization']
+    raise ActiveRecord::RecordNotFound if User.find_by(api_key: auht_token).nil?
 
-    if user.nil?
-      render json: { status: 401 }, status: :unauthorized
-    else
-      render json: { status: 200 }, status: :ok
-    end
-
+    render json: {}, status: :ok
+  rescue => _e
+    render json: { status: 401 }, status: :unauthorized
   end
 
   def create_authorization_token
-    jwt_token = encode(@jwt_body, true, @options)
-
-    if @user.update(api_key: jwt_token)
+    if @user.update(api_key: @token)
       render json: { status: 201, message: "Token created successfully assigned to #{@user.username}", token: @user.api_key }, status: :created
     else
       render json: { status: 422, error: @token.errors.full_messages }, status: :unprocessable_entity
@@ -31,24 +24,16 @@ module Authorizable
   end
 
   def set_key_and_pass_phrase
-    @key = "keyForTokenAuthorizationAndAuthentication"
-    @pass_phrase = "SecretPhraseToAuthenticateAndAuthorizeAPIcalls"
+    @key = @user.id
+    @pass_phrase = @user.username
   end
 
-  def create_encrypted_code
-    @encrypted_code = OpenSSL::HMAC.hexdigest("SHA256", @key, @pass_phrase)
+  def generate_token
+    @token = OpenSSL::HMAC.hexdigest("SHA256", @key, @pass_phrase)
   rescue StandardError => e
     render json: {
       status: 422,
       error: e.message
     }, status: :unprocessable_entity
-  end
-
-  def set_options
-    @options = { secret_key: @encrypted_code, algorithm: 'HS256', exp: 1.day.from_now }
-  end
-
-  def set_jwt_body
-    @jwt_body = { user_id: @user.id, username: @user.username }
   end
 end
